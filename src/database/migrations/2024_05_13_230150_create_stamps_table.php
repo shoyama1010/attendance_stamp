8d7a1e4633e47fe1ff4583ce772feba0648d28c0
shoyama1010 <156488466+shoyama1010@users.noreply.github.com>
2024-05-16 11:32:32 +0900

    Add files via upload

---
+++ src/database/migrations/2024_05_13_230150_create_stamps_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStampsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stamps', function (Blueprint $table) {
+            $table->id();
+            $table->stamp_date('date');
+            $table->start_work('datetime');
+            $table->stop_work('datetime');
+            $table->start_break('datetime');
+            $table->stop_break('datetime');
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stamps');
+    }
+}
